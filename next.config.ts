347602814316e348595318b7ec50fbebde153fe8
Ethan Gramowski <gramo012@umn.edu>
2025-04-21 08:52:57 -0500

    jump. pt 2

--- next.config.ts
+++ next.config.ts
@@ -5,20 +5,7 @@ const nextConfig: NextConfig = {
     ppr: true
   },
 
-  beforeFiles: [
-    // if the host is `app.acme.com`,
-    // this rewrite will be applied
-    {
-      source: '/:path*',
-      has: [
-        {
-          type: 'host',
-          value: 'jump.ethang.earth',
-        },
-      ],
-      destination: '/jump/:path*',
-    },
-  ]
+
 };
 
 export default nextConfig;
