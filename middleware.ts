1d16285d4f884c262e68c1a7d63fb9863f4d2930
Ethan Gramowski <gramo012@umn.edu>
2025-04-21 09:42:38 -0500

    skip static names in middleware

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,17 @@ export function middleware(request: NextRequest) {
     const hostname = request.headers.get('host') || ''
     const subdomain = hostname.split('.')[0]
 
+    const pathname = request.nextUrl.pathname
+
+    // Skip static files and Next.js internals
+    const STATIC_FILE = pathname.startsWith('/_next') ||
+        pathname.startsWith('/favicon.ico') ||
+        pathname.startsWith('/Images') ||
+        pathname.startsWith('/robots.txt') ||
+        pathname.includes('.')
+
+    if (STATIC_FILE) return NextResponse.next()
+
     if (subdomain === 'jump') {
         const url = request.nextUrl.clone()
         url.pathname = `/jump${url.pathname}`
